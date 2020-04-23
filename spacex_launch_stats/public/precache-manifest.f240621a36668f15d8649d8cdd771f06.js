ef86291e2141c385b124b06338cb5a8ecdb65697
Leonardo Costa <leo.costa.programming@gmail.com>
2020-04-23 17:27:42 +0200

    ready to deploy to Heroku

---
+++ spacex_launch_stats/public/precache-manifest.f240621a36668f15d8649d8cdd771f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82be69911f3a110298ac",
+    "url": "/static/js/main.82be6991.chunk.js"
+  },
+  {
+    "revision": "49778ec90a0f7e1f8b13",
+    "url": "/static/js/1.49778ec9.chunk.js"
+  },
+  {
+    "revision": "82be69911f3a110298ac",
+    "url": "/static/css/main.b236e762.chunk.css"
+  },
+  {
+    "revision": "6b38a5d3ab170807eab46a57d4a1bd85",
+    "url": "/index.html"
+  }
+];
